2c5134e145135dff84a797b4de7e7fb033ef60a5
Tella Boluwatife <boluwatifetella@gmail.com>
2022-10-05 07:09:21 +0100

    create args

---
+++ become_an_expert/script/createargs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Purpose: Counting given positional parameters.
+#Version: 1.0
+#Created Date:  Wed Oct 5 07:01:42 BST 2022
+#Modified Date: 
+#Author: Tella Boluwatife
+# START #
+echo "Your current given parameters are $#"
+# END #
