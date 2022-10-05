0b2fd7ab18609be06ba966655f3eab010456e696
Tella Boluwatife <boluwatifetella@gmail.com>
2022-10-05 01:28:29 +0100

    quotes differences

---
+++ become_an_expert/script/quotes.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Purpose: Verifying difference between quotation marks
+#Version: 1.0
+#Created Date:  Wed Oct 5 01:20:13 BST 2022
+#Modified Date: 
+#Author: Tella Boluwatife
+# START #
+VAR1=codeReaper0
+TEST=Tella
+
+# Double quotes ""
+echo "Execute double quotes $VAR1 $TEST"
+
+# Single quotes ''
+echo 'Execute single quotes $VAR1 $TEST'
+
+# Reverse quotes ``
+echo "This Hostname is: `hostname`"
+
+# END #
