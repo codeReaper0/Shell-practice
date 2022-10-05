4f1ee6f92a380bb9faeb1498984c52f34704b539
Tella Boluwatife <boluwatifetella@gmail.com>
2022-10-05 01:19:21 +0100

    created testing templates file

---
+++ become_an_expert/script/helloworld.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Purpose: This is the sample template file
+#Version: 1.0
+#Created Date:  Wed Oct 5 01:17:27 BST 2022
+#Modified Date: 
+#Author: Tella Boluwatife
+# START #
+echo "Testing Template file"
+# END #
