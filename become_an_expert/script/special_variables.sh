318b68ab91239138434b2820b6f5e582dda9582c
Tella Boluwatife <boluwatifetella@gmail.com>
2022-10-05 06:59:49 +0100

    special variables

---
+++ become_an_expert/script/special_variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Purpose: To learn special variables
+#Version: 1.0
+#Created Date:  Wed Oct 5 01:46:16 BST 2022
+#Modified Date: 
+#Author: Tella Boluwatife
+# START #
+echo "'$*' output is $*"
+echo "'$#' output is $#"
+echo "'$1' output is $1"
+echo "'$2' output is $2"
+echo "'$0' output is $0"
+echo "'$@' output is $@"
+echo "'$?' output is $?"
+echo "'$$' output is $$"
+echo "'$!' output is $!"
+# END #
