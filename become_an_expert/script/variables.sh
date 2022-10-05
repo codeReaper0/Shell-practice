533e0179ab3cc9cbe4d057f6ed1db243add4854a
Tella Boluwatife <boluwatifetella@gmail.com>
2022-10-05 01:38:13 +0100

    variables

---
+++ become_an_expert/script/variables.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Purpose: About Variables
+#Version: 1.0
+#Created Date:  Wed Oct 5 01:30:10 BST 2022
+#Modified Date: 
+#Author: Tella Boluwatife
+# START #
+
+#Variable must begin with letters
+#Variables are case sensitive
+#Reserved word not allowed
+
+A=10
+B=23
+HOSTNAME=$(hostname)
+DATE=`date`
+CALENDAR=`cal`
+
+echo "Variable A Value: $A"
+echo "Variable B Value: $B"
+echo "Variable HOSTNAME Value: $HOSTNAME"
+echo "Variable DATE Value: $DATE"
+echo "Variable CALENDAR Value: $CALENDAR"
+# END #
